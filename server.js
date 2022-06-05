eaa03318641c29173d3cc231daabff1fa178d523
Likinus <likinus2@gmail.com>
2022-06-05 18:42:41 +0300

    no message

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 8080;
+const express = require('express');
+
 const app = express();
 
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.set('port', process.env.PORT || 3000);
 
-// send the user to index html page inspite of the url
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './src/index.tsx'));
+const server = app.listen(app.get('port'), function() {
+    console.log('listening on port ', server.address().port);
 });
-
-app.listen(port);
