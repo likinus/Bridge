7b3ab04622a6f5df68b8c202af848caece671832
Likinus <likinus2@gmail.com>
2022-06-05 14:49:52 +0300

    store path changed

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
-import store from "@store/store";
+import store from "../src/store/store";
 
 import App from './App';
 
